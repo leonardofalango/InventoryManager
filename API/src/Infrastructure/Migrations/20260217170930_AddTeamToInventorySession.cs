666e6cf286feff8010b05dd04379c04b8bbb0a62
leonardofalango <leo_falango@hotmail.com>
2026-02-17 14:27:53 -0300

    add migration

---
+++ API/src/Infrastructure/Migrations/20260217170930_AddTeamToInventorySession.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeamToInventorySession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
