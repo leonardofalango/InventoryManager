c080de5ac976db5bfdea5fc6545336228041bdb7
leonardofalango <leo_falango@hotmail.com>
2026-02-17 13:45:30 -0300

    chenge localstorage to sessionstorage and add config

--- UI/src/lib/axios.ts
+++ UI/src/lib/axios.ts
@@ -2,12 +2,21 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5158/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
