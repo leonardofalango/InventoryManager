0e87a573999212c5ee04a390b4387f200fb93538
leonardofalango <leo_falango@hotmail.com>
2026-04-07 18:29:32 -0300

    add http to android schema

--- UI/capacitor.config.ts
+++ UI/capacitor.config.ts
@@ -4,6 +4,9 @@ const config: CapacitorConfig = {
   appId: "com.absoluta.inventory.manager",
   appName: "Absoluta Inventory Manager",
   webDir: "dist",
+  server: {
+    androidScheme: "http",
+  },
 };
 
 export default config;
