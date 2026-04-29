6f97734a3f2da52f1e200a5888ad19139452ae53
leonardofalango <leo_falango@hotmail.com>
2026-04-29 15:47:55 -0300

    fix error 401 handler in axios

--- UI/src/lib/axios.ts
+++ UI/src/lib/axios.ts
@@ -17,6 +17,10 @@ api.interceptors.request.use(
     return config;
   },
   (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
     return Promise.reject(error);
   },
 );
